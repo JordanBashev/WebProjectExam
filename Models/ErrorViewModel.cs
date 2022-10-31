d60bdff8b1c33d048e15a391a3c4814c673f99cf
USER <USER@DESKTOP-53TVEML>
2022-10-31 16:55:04 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebProjectExam.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
