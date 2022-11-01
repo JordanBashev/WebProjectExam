8f5b8fd4c523c2294055ea980aa80bc5faa8b4d1
Vasil <33176648+kokonut79@users.noreply.github.com>
2022-11-01 20:31:07 +0200

    added entity class Shoes

---
+++ Models/Entities/Shoes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace WebProjectExam.Models.Entities
+{
+    public class Shoes
+    {
+        //Foreign key 
+        public int Tag_Id { get; set; }
+        
+        public decimal Price { get; set; }
+        public string Colour { get; set; }
+        public double  Size { get; set; }
+
+        //optional 
+        public string Description { get; set; }
+
+    }
+}
+
